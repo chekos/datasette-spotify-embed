c52363dd0c8b850cfa741766d88114b7ab0e05bf
chekos <chekos@users.noreply.github.com>
2022-08-20 20:37:02 +0000

    Initial library structure

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-spotify-embed",
+    description="Turn Spotify track links into iframe embeds in the Datasette interface",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Sergio Sánchez Zavala",
+    url="https://github.com/chekos/datasette-spotify-embed",
+    project_urls={
+        "Issues": "https://github.com/chekos/datasette-spotify-embed/issues",
+        "CI": "https://github.com/chekos/datasette-spotify-embed/actions",
+        "Changelog": "https://github.com/chekos/datasette-spotify-embed/releases",
+    },
+    license="Apache License, Version 2.0",
+    classifiers=[
+        "Framework :: Datasette",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    version=VERSION,
+    packages=["datasette_spotify_embed"],
+    entry_points={"datasette": ["spotify_embed = datasette_spotify_embed"]},
+    install_requires=["datasette"],
+    extras_require={"test": ["pytest", "pytest-asyncio"]},
+    python_requires=">=3.7",
+)
